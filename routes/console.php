b0aec180d43c0ee1cad79ba36bc5fbf793c1bb0e
Aleh Kliuiko <o.kliuiko@gmail.com>
2024-11-11 15:34:36 +0300

    Добавление функционала отмены не оплаченных заказов каждые 2 минуты

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,23 @@
 <?php
 
+use App\Enums\OrderStatus;
+use App\Models\Order;
 use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+
+    $cutoffTime = Carbon::now()->subMinutes(2);
+
+    Order::query()
+        ->where('status', OrderStatus::PENDING->value)
+        ->where('created_at', '<=', $cutoffTime)
+        ->update(['status' => OrderStatus::CANCELED->value]);
+
+})->everyMinute();
